86be8e01f0a4c6842dcb62c863df38cf90b2e3f6
andomingos87 <andomingos87@gmail.com>
2025-05-10 12:44:49 -0300

    Aumenta limite upload para 1gb

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'football-vision-analytics-secret-key'
     app.config['UPLOAD_FOLDER'] = 'app/static/uploads'
     app.config['RESULTS_FOLDER'] = 'app/static/results'
-    app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024  # 500MB max upload size
+    app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024  # 1GB max upload size
     app.config['ALLOWED_EXTENSIONS'] = {'mp4', 'mov'}
     
     from .routes import main
